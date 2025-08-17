c717dcf5227ad411561af755d9dcd1f81176f53b
Yuri Vital <vital.systm@gmail.com>
2025-08-17 20:15:29 +0000

    chore: Create Elixir App and basic ci

---
+++ scheduler/lib/scheduler.ex
@@ -0,0 +1,18 @@
+defmodule Scheduler do
+  @moduledoc """
+  Documentation for `Scheduler`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Scheduler.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
